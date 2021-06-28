49d23490a4f6e3ea2a1f36cc9feca0912389e4fb
Abiola Baruwa <abiola.baruwa.business@gmail.com>
2021-06-28 17:26:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-// write your code below!
-function happyHolidays() {
-  return "Happy holidays!"
-}
 
-function happyHolidayTo (name) {
-  return `Happy holidays, ${name}!`
+var happyHolidays =  function(){
+  return "Happy holidays!";
 }
-
-function happyCustomHolidayTo (holiday, name) {
-  return `Happy ${holiday}, ${name}!`
+var happyHolidaysTo =  function(name){
+  return "Happy holidays, " + name + "!";
 }
-
+var happyHolidayTo =  function(holiday, name){
+  return "Happy " + holiday + ", " + name + "!";
+}
+var holidayCountdown =  function(holiday, days){
+  return  "It's " + days + " days until " + holiday + "!";
+}
 function holidayCountdown(holiday, days) {
   return `It's ${days} days until ${holiday}!`
 }
